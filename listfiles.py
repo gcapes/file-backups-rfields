7609a686ab677f9830afbc226be1001ef2196cf1
Gerard Capes <gerard.capes@manchester.ac.uk>
2018-01-25 14:56:11 +0000

    Descend into directories
    Code was not listing the contents of subdirectories.
    
    Fix #1.

--- listfiles.py
+++ listfiles.py
@@ -3,12 +3,12 @@ import os
 def listFilesAndDirs(path,allFiles):
     for fileOrDir in os.listdir(path):
         if not fileOrDir.startswith('.'):
-            if os.path.isdir(fileOrDir):
+            fullPath = os.path.join(path,fileOrDir)
+            if os.path.isdir(fullPath):
                 listFilesAndDirs(os.path.join(path,fileOrDir),allFiles)
             else:
                 allFiles.append(os.path.join(path,fileOrDir))
     return allFiles
             
-for x in listFilesAndDirs('.',[]):
+for x in listFilesAndDirs('../',[]):
     print(x)
-listFilesAndDirs('.',[])
