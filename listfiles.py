d6d8379ab790d06700f6526b4efd37dfd6e648b4
Gerard Capes <gerard.capes@manchester.ac.uk>
2018-01-26 12:01:35 +0000

    Add function to back up a directory
    This is just a test. I'm not yet sure whether I'll need this,
    or whether I'll just copy files using a src and dest name list.

--- listfiles.py
+++ listfiles.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 def getSrcFiles(path,allFiles):
     for fileOrDir in os.listdir(path):
@@ -12,3 +13,18 @@ def getSrcFiles(path,allFiles):
             
 for x in getSrcFiles('../',[]):
     print(x)
+
+def backupDir(src,dest):
+    assert(os.path.isdir(src))
+    srcDir = os.path.abspath(src)
+    srcFiles = os.listdir(srcDir)
+
+    if not os.path.exists(dest):
+        os.makedirs(dest)
+    assert(os.path.isdir(dest))
+
+    for file in srcFiles:
+        if not file.startswith('.'):
+            shutil.copy(file,dest)
+
+backupDir('.','/home/mbexegc2/Downloads/pytest/')
