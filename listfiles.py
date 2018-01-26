a58324b62534cc8e5defb24ae811f55c87a2185c
Gerard Capes <gerard.capes@manchester.ac.uk>
2018-01-26 11:58:32 +0000

    Rename function

--- listfiles.py
+++ listfiles.py
@@ -1,14 +1,14 @@
 import os
 
-def listFilesAndDirs(path,allFiles):
+def getSrcFiles(path,allFiles):
     for fileOrDir in os.listdir(path):
         if not fileOrDir.startswith('.'):
             fullPath = os.path.join(path,fileOrDir)
             if os.path.isdir(fullPath):
-                listFilesAndDirs(os.path.join(path,fileOrDir),allFiles)
+                getSrcFiles(os.path.join(path,fileOrDir),allFiles)
             else:
                 allFiles.append(os.path.join(path,fileOrDir))
     return allFiles
             
-for x in listFilesAndDirs('../',[]):
+for x in getSrcFiles('../',[]):
     print(x)
