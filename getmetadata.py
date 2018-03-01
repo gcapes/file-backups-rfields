fe2f86f681475910b82094bd7464859704202176
Gerard Capes <gerard.capes@manchester.ac.uk>
2018-03-01 15:00:59 +0000

    Improve formatting of regex results
    Make search case-insensitive.
    Return matched group and combine with keyword -- returning whole match
    gives inconsistent capitalisation and no spacing around keyword.

--- getmetadata.py
+++ getmetadata.py
@@ -6,17 +6,18 @@ def getmetadata(inputFile,keywordList):
     with open(inputFile,'r', encoding='ascii',errors='ignore') as f:
         for line in f:
             for keyword in keywordList:
-                regex = keyword + '=\w+'
-                match = re.search(regex,line)
+                regex = keyword + '=(\w+)'
+                match = re.search(regex,line,re.IGNORECASE)
                 if match:
-                    metadata.append(match.group(0))
+                    metadata.append(keyword + ' = ' + match.group(1))
                     break # Not expecting more than one keyword per line
     return metadata
 
 CCDfile = 'Ivium Datafile - CCCD Example.idf'
 CVfile = 'Ivium Datafile - CV Example.ids'
 EISfile = 'Ivium Datafile - EIS Example.idf'
-keywords = ['Method','serialnumber']
+keywords = ['Serialnumber','Software','Firmware','Technique']
+
 print(getmetadata(CCDfile,keywords))
 print(getmetadata(CVfile,keywords))
 print(getmetadata(EISfile,keywords))
