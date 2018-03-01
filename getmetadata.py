2a8bdbf2c058c3e4f9181761327121b49cf53464
Gerard Capes <gerard.capes@manchester.ac.uk>
2018-03-01 15:38:31 +0000

    Create readme file from list
    Add functions to:
    - create parent directories if missing
    - write a list into a file

--- getmetadata.py
+++ getmetadata.py
@@ -1,4 +1,5 @@
 import re
+import os
 
 def getmetadata(inputFile,keywordList):
     '''Extract key info from (.idf and .ids) log files'''
@@ -13,11 +14,36 @@ def getmetadata(inputFile,keywordList):
                     break # Not expecting more than one keyword per line
     return metadata
 
+def writelisttofile(data,filename):
+    '''Write contents of <data> into <filename>'''
+    # Check parent directory of file exists
+    createdirfromfilepath(filename)
+
+    file = open(filename,'w')
+    for i, value in enumerate(data):
+        file.write('%s\n' % value)
+
+def createdirfromfilepath(dest):
+    '''If dest dir doesn't exist, create it'''
+    destDir = os.path.dirname(os.path.abspath(dest))
+    if not os.path.exists(destDir):
+        os.makedirs(destDir)
+    # Confirm destination directory exists before copying
+    assert(os.path.exists(destDir))
+
+# Testing
 CCDfile = 'Ivium Datafile - CCCD Example.idf'
 CVfile = 'Ivium Datafile - CV Example.ids'
 EISfile = 'Ivium Datafile - EIS Example.idf'
 keywords = ['Serialnumber','Software','Firmware','Technique']
 
-print(getmetadata(CCDfile,keywords))
-print(getmetadata(CVfile,keywords))
-print(getmetadata(EISfile,keywords))
+CCDlist = getmetadata(CCDfile,keywords)
+CVlist = getmetadata(CVfile,keywords)
+EISlist = getmetadata(EISfile,keywords)
+
+CCDReadmeFile = os.path.join('output','CCD','README.txt')
+CVReadmeFile = os.path.join('output','CV','README.txt')
+EISReadmeFile = os.path.join('output','EIS','README.txt')
+writelisttofile(CCDlist,CCDReadmeFile)
+writelisttofile(CVlist,CVReadmeFile)
+writelisttofile(EISlist,EISReadmeFile)
