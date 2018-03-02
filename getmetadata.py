64b0f6191591dddc27a97e2ac002c8b62780a1c7
Gerard Capes <gerard.capes@manchester.ac.uk>
2018-03-02 10:48:20 +0000

    Reformat code with pycharm

--- getmetadata.py
+++ getmetadata.py
@@ -1,27 +1,30 @@
 import re
 import os
 
-def getmetadata(inputFile,keywordList):
+
+def getmetadata(inputFile, keywordList):
     '''Extract key info from (.idf and .ids) log files'''
     metadata = []
-    with open(inputFile,'r', encoding='ascii',errors='ignore') as f:
+    with open(inputFile, 'r', encoding='ascii', errors='ignore') as f:
         for keyword in keywordList:
             regex = keyword + '=(\w{2,})'
             for line in f:
-                match = re.search(regex,line,re.IGNORECASE)
+                match = re.search(regex, line, re.IGNORECASE)
                 if match:
                     metadata.append(keyword + ' = ' + match.group(1))
-                    break # Not expecting more than one keyword per line
+                    break  # Not expecting more than one keyword per line
     return metadata
 
-def writelisttofile(data,filename):
+
+def writelisttofile(data, filename):
     '''Write contents of <data> into <filename>'''
     # Check parent directory of file exists
     createdirfromfilepath(filename)
 
-    file = open(filename,'w')
+    file = open(filename, 'w')
     for i, value in enumerate(data):
         file.write('%s\n' % value)
+
 
 def createdirfromfilepath(dest):
     '''If dest dir doesn't exist, create it'''
@@ -29,21 +32,22 @@ def createdirfromfilepath(dest):
     if not os.path.exists(destDir):
         os.makedirs(destDir)
     # Confirm destination directory exists before copying
-    assert(os.path.exists(destDir))
+    assert (os.path.exists(destDir))
+
 
 # Testing
 CCDfile = 'Ivium Datafile - CCCD Example.idf'
 CVfile = 'Ivium Datafile - CV Example.ids'
 EISfile = 'Ivium Datafile - EIS Example.idf'
-keywords = ['Serialnumber','Software','Firmware','Technique']
+keywords = ['Serialnumber', 'Software', 'Firmware', 'Technique']
 
-CCDlist = getmetadata(CCDfile,keywords)
-CVlist = getmetadata(CVfile,keywords)
-EISlist = getmetadata(EISfile,keywords)
+CCDlist = getmetadata(CCDfile, keywords)
+CVlist = getmetadata(CVfile, keywords)
+EISlist = getmetadata(EISfile, keywords)
 
-CCDReadmeFile = os.path.join('output','CCD','README.txt')
-CVReadmeFile = os.path.join('output','CV','README.txt')
-EISReadmeFile = os.path.join('output','EIS','README.txt')
-writelisttofile(CCDlist,CCDReadmeFile)
-writelisttofile(CVlist,CVReadmeFile)
-writelisttofile(EISlist,EISReadmeFile)
+CCDReadmeFile = os.path.join('output', 'CCD', 'README.txt')
+CVReadmeFile = os.path.join('output', 'CV', 'README.txt')
+EISReadmeFile = os.path.join('output', 'EIS', 'README.txt')
+writelisttofile(CCDlist, CCDReadmeFile)
+writelisttofile(CVlist, CVReadmeFile)
+writelisttofile(EISlist, EISReadmeFile)
--- listfiles.py
+++ listfiles.py
@@ -1,24 +1,26 @@
 import os
 import shutil
 
-def getSrcFiles(path,allFiles):
+
+def getSrcFiles(path, allFiles):
     for fileOrDir in os.listdir(path):
         if not fileOrDir.startswith('.'):
-            fullPath = os.path.join(path,fileOrDir)
+            fullPath = os.path.join(path, fileOrDir)
             if os.path.isdir(fullPath):
-                getSrcFiles(os.path.join(path,fileOrDir),allFiles)
+                getSrcFiles(os.path.join(path, fileOrDir), allFiles)
             else:
-                allFiles.append(os.path.join(path,fileOrDir))
+                allFiles.append(os.path.join(path, fileOrDir))
     return allFiles
-            
-def backupDir(src,dest):
-    assert(os.path.isdir(src))
+
+
+def backupDir(src, dest):
+    assert (os.path.isdir(src))
     srcDir = os.path.abspath(src)
     srcFiles = os.listdir(srcDir)
 
     if not os.path.exists(dest):
         os.makedirs(dest)
-    assert(os.path.isdir(dest))
+    assert (os.path.isdir(dest))
 
     for file in srcFiles:
         # Don't copy hidden files
@@ -26,33 +28,36 @@ def backupDir(src,dest):
             # Don't overwrite destination files
             destFile = os.path.abspath(file)
             if not os.path.exists(destFile):
-                shutil.copy(file,dest)
+                shutil.copy(file, dest)
 
-def backupFiles(srcFiles,destFiles):
+
+def backupFiles(srcFiles, destFiles):
     # srcFiles is a list of source files
     # destFiles is a corresponding list of destination files
-    assert(len(srcFiles) == len(destFiles))
+    assert (len(srcFiles) == len(destFiles))
 
     for src, dest in zip(srcFiles, destFiles):
         # Check that src exists
-        assert(os.path.exists(src)), "Source file doesn't exist: %r" % src
+        assert (os.path.exists(src)), "Source file doesn't exist: %r" % src
 
         # If dest dir doesn't exist, create it
         destDir = os.path.dirname(os.path.abspath(dest))
         if not os.path.exists(destDir):
             os.makedirs(destDir)
         # Confirm destination directory exists before copying
-        assert(os.path.exists(destDir))
+        assert (os.path.exists(destDir))
 
         # Check that destination file doesn't already exist
         if not os.path.exists(dest):
-            shutil.copyfile(src,dest)
+            shutil.copyfile(src, dest)
         else:
             print('Destination file already exists: %r. File not copied.' % dest)
         # Confirm file has copied
-        assert(os.path.exists(dest)), "File failed to copy. Src: %r, Dest: %r" % (src, dest)
+        assert (os.path.exists(dest)), "File failed to copy. Src: %r, Dest: %r" % (src, dest)
 
-for x in getSrcFiles('../',[]):
+
+for x in getSrcFiles('../', []):
     print(x)
-backupDir('.','/home/mbexegc2/Downloads/pytest/')
-backupFiles(['listfiles.py','README.md'],['/home/mbexegc2/Downloads/pytest/pythonfile.py','/home/mbexegc2/Downloads/pytest/dir/markdown.md'])
+backupDir('.', '/home/mbexegc2/Downloads/pytest/')
+backupFiles(['listfiles.py', 'README.md'],
+            ['/home/mbexegc2/Downloads/pytest/pythonfile.py', '/home/mbexegc2/Downloads/pytest/dir/markdown.md'])
