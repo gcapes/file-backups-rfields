29bedb359c1e31f9157433590d5adff153750432
Gerard Capes <gerard.capes@manchester.ac.uk>
2018-03-09 13:19:51 +0000

    Add thoughts to testing script

--- testbackup.py
+++ testbackup.py
@@ -1,8 +1,12 @@
 # Test code during development.
 import logsheet as ls
 
-ls.createlogsheet('/home/gerard/Downloads/temp')
-ls.copydirusinglogsheet('/home/gerard/Downloads/temp/logsheet.txt','/home/gerard/backup')
-logsheetlog = ls.findlogsheets('/home/gerard/Downloads', 'logsheet.txt')
+ls.createlogsheet('/home/mbexegc2/Downloads/test/subinclude')
+# Make an error with logsheet path here to test assertion
+ls.copydirusinglogsheet('/home/mbexegc2/Downloads/test/subinclude/logsheet.txt','/home/mbexegc2/backup')
+logsheetlog = ls.findlogsheets('/home/mbexegc2/Downloads', 'logsheet.txt')
 print('With logsheet: ',logsheetlog[0])
-print('Without logsheet: ',logsheetlog[1])
+print('Missing logsheet: ',logsheetlog[1])
+# Check that directories have been backed up
+# Read logsheets
+# Check destination directories and files exist, and that the file sizes are the same.
