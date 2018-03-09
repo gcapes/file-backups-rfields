f3c2b1fbee1fd11390ff95ac3c527ceaf622400f
Gerard Capes <gerard.capes@manchester.ac.uk>
2018-03-09 15:59:21 +0000

    Rework test script into intended workflow

--- testbackup.py
+++ testbackup.py
@@ -1,12 +1,31 @@
 # Test code during development.
 import logsheet as ls
+import utils
+import os
 
-ls.createlogsheet('/home/mbexegc2/Downloads/test/subinclude')
-# Make an error with logsheet path here to test assertion
-ls.copydirusinglogsheet('/home/mbexegc2/Downloads/test/subinclude/logsheet.txt','/home/mbexegc2/backup')
-logsheetlog = ls.findlogsheets('/home/mbexegc2/Downloads', 'logsheet.txt')
-print('With logsheet: ',logsheetlog[0])
-print('Missing logsheet: ',logsheetlog[1])
+# Define variables
+datadir      = '/home/mbexegc2/Downloads'
+backupdir    = '/home/mbexegc2/backup'
+missinglog   = os.path.join(datadir,'missinglogsheets.txt')
+logsheetname = 'logsheet.txt'
+tobecopied   = os.path.join(datadir, 'dirstobecopied.txt')
+
+# Create README files from .idf and .ids data files
+
+logsheetreport = ls.findlogsheets('/home/mbexegc2/Downloads', logsheetname)
+
+dirswithlogsheet    = logsheetreport[0]
+dirsmissinglogsheet = logsheetreport[1]
+
+utils.writelisttofile(dirsmissinglogsheet, missinglog)
+utils.writelisttofile(dirswithlogsheet, tobecopied)
+
+for dir in dirswithlogsheet:
+    logsheet = os.path.join(dir, logsheetname)
+    ls.copydirusinglogsheet(logsheet, backupdir)
+    # Write log file to report src and dest directories and time copied
+    
 # Check that directories have been backed up
-# Read logsheets
-# Check destination directories and files exist, and that the file sizes are the same.
+    # Check destination directories and files exist, and that the file sizes are the same.
+
+# Read missinglogsheets.txt file in order to create new logsheets.
