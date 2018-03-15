30c0431675fc3e48c881fa3f87c25757801c4f9e
Gerard Capes <gerard.capes@manchester.ac.uk>
2018-03-15 15:29:13 +0000

    Add function to write metadata README file

--- getmetadata.py
+++ getmetadata.py
@@ -20,6 +20,38 @@ def getmetadata(inputFile, keywordList):
                     break  # Not expecting more than one keyword per line
     return metadata
 
+def getdatafile(dir,extensions):
+    """
+    Get name of data file, given directory name.
+    :param: dir: Directory to search
+    :param: extensions: Data file file extensions (string or tuple)
+    :return: file
+    """
+    
+    assert os.path.exists(dir), "Directory doesn't exist: %s" % dir
+    
+    dirlisting = os.listdir(dir)
+    for file in dirlisting:
+        if file.endswith(extensions):
+            break
+    else:
+        raise NameError('File not found. Looking for a file ending in %s in %s.' % (extensions, dir))
+    
+    return file
+
+def writereadme(dir, ext, keywords):
+    """
+    Write README file including key information from data file.
+    param: dir: Source directory containing the data file.
+    param: ext: File extensions to search for.
+    param: keywords: Information to extract from data file.
+    """
+    assert os.path.exists(dir), "Directory doesn't exist: %s" % dir
+    
+    file = getdatafile(dir, ext)
+    data = getmetadata(file, keywords)
+    readme = os.path.join(dir, 'README.txt')
+    utils.writelisttofile(data, readme)
 
 # Testing
 CCDfile = 'Ivium Datafile - CCCD Example.idf'
