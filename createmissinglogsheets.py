f872a2ece2425823847eef51846a591f6dee217a
Gerard Capes <gerard.capes@manchester.ac.uk>
2018-03-16 09:02:57 +0000

    Create missing log sheets from log file (manually)
    Fix #4.

---
+++ createmissinglogsheets.py
@@ -0,0 +1,12 @@
+# Prompt user for information on all missing logfiles
+import logsheet as ls
+import os
+
+missinglogs = "/home/mbexegc2/Downloads/missinglogsheets.txt"
+
+assert os.path.exists(missinglogs), "File not found: %s" % missinglogs
+
+with open(missinglogs, 'r') as f:
+    for line in f:
+        dir = line.strip('\n')
+        ls.createlogsheet(dir)
